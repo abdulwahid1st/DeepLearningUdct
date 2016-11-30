5e878e795bb2cce9f1f65f6fbc4ddae020bc5d8a
abdul-february-mark2 <abdulwahid@february-mark2.pc>
2016-11-30 07:15:07 +0700

    task 1

---
+++ lesson1/task1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a = 1000000000
+b = 0.000001
+c = -1000000000
+
+for i in range(1000000):
+    a = a+b
+d = a+c
+
+print(d)
